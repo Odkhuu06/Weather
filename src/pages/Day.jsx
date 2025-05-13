367e29b6b85906bbce3c3c72de8c7af5093a5bef
25LP2339 25LP2339 <25LP2339@pinecones-iMac-2.local>
2025-05-13 12:36:36 +0800

    1

---
+++ src/pages/Day.jsx
@@ -0,0 +1,16 @@
+export const Day = () => {
+  return (
+    <div className="w-[800px] h-[1200px] bg-[#F3F4F6]">
+      <div className="pl-[40px] pt-[40px]">
+        <input
+          placeholder="Search"
+          className="w-lg h-20 bg-stone-50 min-w-xs text-zinc-950 shadow-lg"
+        />
+      </div>
+      <div className="z-20 w-[414px] h-[832px] rounded-10.5 rounded-4xl overflow-hidden shadow-lg bg-[#FFFFFF]/75 backdrop-blur-md"></div>
+    </div>
+  );
+};
+{
+  Day;
+}
---
+++ src/pages/Night.jsx
@@ -0,0 +1,8 @@
+export const Night = () => {
+  return (
+    <div className="w-[800px] h-[1200px] bg-[#0F141E] flex justify-center">
+      <div className="z-20 w-[414px] h-[832px] rounded-10.5 rounded-4xl overflow-hidden shadow-lg bg-[#111827]/75 backdrop-blur-md"></div>
+    </div>
+  );
+};
+{Night}
